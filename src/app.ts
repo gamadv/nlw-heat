fbe226dd07cd612157c5d3662861d7ae8cc5bf65
Moacir Gama Leal Júnior <lealdev.py@gmail.com>
2021-10-19 22:20:13 -0300

    fix: add missing cors use

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,15 @@
 import 'dotenv/config';
 import express from 'express';
-import { Server } from 'socket.io';
 import http from 'http';
 import cors from 'cors';
+import { Server } from 'socket.io';
+
 import { router } from './routes';
 
 const app = express();
+app.use(cors());
 
 const serverHttp = http.createServer(app);
-
 const io = new Server(serverHttp, {
   cors: {
     origin: '*',
